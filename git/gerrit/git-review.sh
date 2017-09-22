9685f831d16300c4dfaf33af627c36ef49bb3f49
Gilles Legoux <gilles.legoux@gmail.com>
2017-09-22 19:23:57 +0200

    Update git-review.sh

--- git/gerrit/git-review.sh
+++ git/gerrit/git-review.sh
@@ -2,26 +2,88 @@
 
 set -e
 
-# go to repository home
-cd "$(git rev-parse --show-toplevel)"
+# constant
 
-current_branch="$(git rev-parse --abbrev-ref HEAD)"
-#message_commit="$(git log --format=%B -n 1)"
-message_commit=$(cat .git/COMMIT_EDITMSG)
-change_id="$(echo "${message_commit}" | grep -E "^Change-Id:" | cut -f2 -d " ")"
+GERRIT_URL="" # TO COMPLETE
 
-if [[ -z "${change_id}" ]] || [[ $(echo "${change_id}" | wc -l) -ne 1 ]]; then
-  >&2 echo "ERROR: Incorrect Change-Id: '${change_id}'"
-  exit 1
+# helper function
+
+get_info_gerrit() {
+  local change_id="$1"
+  curl -s "${GERRIT_URL}/changes/${change_id}/?o=CURRENT_REVISION" | sed 1d
+}
+
+get_change_number() {
+    local result="$1"
+    echo "${result}" | jq ._number
+}
+
+get_commit_id() {
+    local result="$1"
+    echo "${result}" | jq .current_revision | sed 's/"//g'
+}
+
+get_n_patch_sets() {
+    local result="$1"
+    local commit_id="$2"
+    echo "${result}" | jq .revisions."${commit_id}"._number
+}
+
+# script
+
+if [[ "${FUNCNAME[0]}" == "main" ]]; then
+
+  # go to repository home
+  cd "$(git rev-parse --show-toplevel)"
+
+  current_branch="$(git rev-parse --abbrev-ref HEAD)"
+  message_commit="$(git log --format=%B -n 1)"
+  change_id="$(echo "${message_commit}" | grep -E "^Change-Id:" | cut -f2 -d " ")"
+
+  if [[ -z "${change_id}" ]] || [[ $(echo "${change_id}" | wc -l) -ne 1 ]]; then
+    >&2 echo "ERROR: Incorrect Change-Id: '${change_id}'"
+    exit 1
+  fi
+
+  # print commit log
+  git log --stat --pretty=fuller -n 1
+  echo
+
+  result="$(get_info_gerrit "${change_id}")"
+  change_number="$(get_change_number "${result}")"
+  commit_id="$(get_commit_id "${result}")"
+  n_patch_sets="$(get_n_patch_sets "${result}" "${commit_id}")"
+
+  echo "See your current changes: ${GERRIT_URL}/${change_number}/"
+  echo "commit id    : ${commit_id}"
+  echo "change number: ${change_number}"
+  echo "change id    : ${change_id}"
+  echo "patch sets   : ${n_patch_sets}"
+  echo
+
+  read -p "Do you want push one review on ${current_branch} branch ? [Y/n]: " \
+    answer
+  if [[ "${answer}" != "Y" ]]; then
+    exit 0
+  fi
+
+  # Possible reference types:
+  # changes, for=publish, draft (deprecated)
+  #
+  # Push with bypassing Code Review:
+  # git push origin HEAD:master
+  # If pushing to Gerrit fails consult the Gerrit documentation that
+  # explains the error messages.
+
+  ref_type="changes"
+
+  if [[ "${ref_type}" == "publish" ]]; then
+    git push origin HEAD:refs/"${ref_type}"/"${1:-${current_branch}}"
+  elif [[ "${ref_type}" == "changes" ]]; then
+    git push origin HEAD:refs/"${ref_type}"/"${1:-${change_number}}"
+  else
+    >&2 echo "ERROR: Unknown reference type: '${ref_type}'"
+    exit 1
+  fi
+
 fi
-
-# print log commit
-git log --stat --pretty=fuller -n 1
-echo
-
-read -p "Do you want push one review ? [Y/n]: " answer
-if [[ "${answer}" != "Y" ]]; then
-  exit 0
-fi
-
-git push origin HEAD:refs/changes/"${1:-${change_id}}"
